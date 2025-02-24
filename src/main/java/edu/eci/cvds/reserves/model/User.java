1ff2f4374ac4db9454c139fad3a28111d4cc459f
Andr3xDev <achavarropp@gmail.com>
2025-02-23 23:28:20 -0500

    Add basic model

--- src/main/java/edu/eci/cvds/reserves/model/User.java
+++ src/main/java/edu/eci/cvds/reserves/model/User.java
@@ -1,5 +1,23 @@
 package edu.eci.cvds.reserves.model;
 
-public class User {
+import java.sql.Date;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+public class User {
+    @Document();
+    @Id
+    private int id;
+    private String name;
+    private String passwd;
+    private String mail;
+    private String type;
+    private String status;
+    private Date register;
+
+    public User(String userName, String passwd) {
+        this.name = userName;
+        this.passwd = passwd;
+    }
 }
