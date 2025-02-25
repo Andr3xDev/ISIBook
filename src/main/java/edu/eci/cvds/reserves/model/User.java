ad4b6ba2fc53b1c9b522c40a7e7033c43f277dbb
Andr3xDev <achavarropp@gmail.com>
2025-02-25 13:48:58 -0500

    Update getters & setters

--- src/main/java/edu/eci/cvds/reserves/model/User.java
+++ src/main/java/edu/eci/cvds/reserves/model/User.java
@@ -1,11 +1,10 @@
 package edu.eci.cvds.reserves.model;
 
-import java.sql.Date;
+import java.sql.Date; //Eliminate after complete the time bug
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-//TODO: Finish date attribute
 //TODO: Contect BD
 
 //@Document();
@@ -26,5 +25,63 @@ public class User {
         this.mail = mail;
         this.type = type;
         this.status = "Active";
+        this.register = new Date(id); // TODO: Set correct date
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getRegister() {
+        return register;
+    }
+
+    public void setRegister(Date register) {
+        this.register = register;
+    }
+
 }
