0d7af3c55a511eb67653cd6c1d1433490423b153
Andr3xDev <achavarropp@gmail.com>
2025-03-03 02:00:27 -0500

    Add controller

--- src/main/java/edu/eci/cvds/reserves/controller/UserController.java
+++ src/main/java/edu/eci/cvds/reserves/controller/UserController.java
@@ -1,5 +1,45 @@
 package edu.eci.cvds.reserves.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import edu.eci.cvds.reserves.model.User;
+import edu.eci.cvds.reserves.service.UserService;
+
+import java.lang.foreign.Linker.Option;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAllUser();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable String id) {
+        return userService.findUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable String id, @RequestBody User user) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+
+    }
 
 }
