a931e2b33b3629b365ec74db4574a49dfea6d972
daviespr1406 <david.espinosa-r@mail.escuelaing.edu.co>
2025-03-10 10:17:02 -0500

    Fix sonar :D

--- src/main/java/edu/eci/cvds/reserves/utils/validations/UserValidation.java
+++
@@ -1,7 +0,0 @@
-package edu.eci.cvds.reserves.utils.validations;
-
-public class UserValidation {
-    public static boolean isValidEmail(String email) {
-        return email != null && email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}");
-    }
-}
