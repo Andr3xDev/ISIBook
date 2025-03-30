43e3d6e2f751f8e88c22b14bfd0c3a368982f283
Andr3xDev <achavarropp@gmail.com>
2025-03-30 01:34:45 -0500

    fix

--- src/main/java/edu/eci/cvds/reserves/config/JwtService.java
+++ src/main/java/edu/eci/cvds/reserves/config/JwtService.java
@@ -6,7 +6,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
-import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 import java.util.function.Function;
@@ -14,15 +13,16 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "CiclosDeVidaDelDesarrolloDeSoftware20251";
+    private static final String SECRET_KEY = "b2xH4jU9kLz5qC8xM2tA7sV6rNwP3yFg";
 
     private Key getSigningKey() {
-        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
     }
 
-    public String generateToken(String username) {
+    public String generateToken(String username, String role) {
         return Jwts.builder()
                 .setSubject(username)
+                .claim("role", role)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
